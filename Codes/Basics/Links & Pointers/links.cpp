c23f4da61e4b684f60656935d72d6583ec377be6
isHardCoded <ishardcoded@gmail.com>
2025-04-26 16:10:42 +0500

    Added Links & Pointers folder and files

---
+++ Codes/Basics/Links & Pointers/links.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	cout << "Value a: " << a << endl;
+	cout << "Address a: " << &a << endl;
+
+	int b = 10;
+	cout << "Value b: " << b << endl;
+	cout << "Address b: " << &b << endl;
+}
